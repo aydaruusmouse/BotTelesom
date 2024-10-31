14a55f609b9660d5fbe7d0ad830073166a902ba6
aydarous mouse <halalmedia9@gmail.com>
2024-10-31 13:14:23 +0300

    update route34321s

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WhatsAppBotController;
 use App\Http\Controllers\SimDetailsController;
 
-Route::get('/sim-details', [SimDetailsController::class, 'showForm'])->name('sim.details.form');
-Route::post('/sim-details', [SimDetailsController::class, 'submitForm'])->name('sim.details.submit');
 
 
 
